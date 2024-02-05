b6dbd0d8292ca94729aa04c91ff75ff452adde67
Parth Gorde <109735469+ParthGorde@users.noreply.github.com>
2024-02-05 18:44:34 +0530

    Update simple_test.py

--- apps/simple_test.py
+++ apps/simple_test.py
@@ -1,13 +1,7 @@
-# Copyright (c) Facebook, Inc. and its affiliates. All rights reserved.
-
-
 from .recon import reconWrapper
 import argparse
 
 
-###############################################################################################
-##                   Setting
-###############################################################################################
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', '--input_path', type=str, default='./sample_images')
 parser.add_argument('-o', '--out_path', type=str, default='./results')
@@ -15,9 +9,7 @@ parser.add_argument('-c', '--ckpt_path', type=str, default='./checkpoints/pifuhd
 parser.add_argument('-r', '--resolution', type=int, default=512)
 parser.add_argument('--use_rect', action='store_true', help='use rectangle for cropping')
 args = parser.parse_args()
-###############################################################################################
-##                   Upper PIFu
-###############################################################################################
+
 
 resolution = str(args.resolution)
 
