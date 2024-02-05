f5fae8c2f04203efeebf6d613782a6f0a12883b4
Parth Gorde <109735469+ParthGorde@users.noreply.github.com>
2024-02-05 18:42:12 +0530

    Update download_trained_model.sh

--- scripts/download_trained_model.sh
+++ scripts/download_trained_model.sh
@@ -1,7 +1,4 @@
-# Copyright (c) Facebook, Inc. and its affiliates. All rights reserved.
-
 set -ex
-
 mkdir -p checkpoints
 cd checkpoints
 wget "https://dl.fbaipublicfiles.com/pifuhd/checkpoints/pifuhd.pt" pifuhd.pt
